6e3c04f4dac81561e87f5b84c84548dead639e3d
David Hagerty <hagerty9009@gmail.com>
2015-02-16 11:17:31 -0500

    Addresses #1 [Swift] Added four buttons and update functions. Four buttons have been added to the app, along with update functions that update a label upon tap. There is also an outlet for a timer that will be implemented soon.

--- Watch The Corners WatchKit Extension/InterfaceController.swift
+++ Watch The Corners WatchKit Extension/InterfaceController.swift
@@ -11,21 +11,28 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
-
-    override func awakeWithContext(context: AnyObject?) {
-        super.awakeWithContext(context)
-        
-        // Configure interface objects here.
-    }
-
-    override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
-        super.willActivate()
-    }
-
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
-    }
-
+  
+  @IBOutlet weak var label: WKInterfaceLabel!
+  
+  @IBOutlet weak var coundownTimer: WKInterfaceTimer!
+  
+  @IBAction func TapOne() {
+    label.setText("One was tapped.")
+  }
+  @IBAction func TapTwo() {
+    label.setText("Two was tapped.")
+  }
+  @IBAction func TapThree() {
+    label.setText("Three was tapped.")
+  }
+  @IBAction func TapFour() {
+    label.setText("Four was tapped.")
+  }
+  func update() {
+    // This will be the timer loop.
+  }
+  override func awakeWithContext(context: AnyObject?) {
+    super.awakeWithContext(context)
+    // Configure interface objects here.
+  }
 }
