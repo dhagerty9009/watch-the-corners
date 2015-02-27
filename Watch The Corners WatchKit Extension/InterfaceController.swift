c94daab781b991abd42dd5de1cccd65c50a392a5
David Hagerty <hagerty9009@gmail.com>
2015-02-27 15:38:23 -0500

    [Swift] Making buttons interactive

--- Watch The Corners WatchKit Extension/InterfaceController.swift
+++ Watch The Corners WatchKit Extension/InterfaceController.swift
@@ -12,27 +12,93 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
   
+  let WHITE: UIColor = UIColor.whiteColor()
+  let BLACK: UIColor = UIColor.blackColor()
+  
+  var random: NSNumber!
+  var highScore: NSNumber!
+  
   @IBOutlet weak var label: WKInterfaceLabel!
   
-  @IBOutlet weak var coundownTimer: WKInterfaceTimer!
+  @IBOutlet weak var buttonOne: WKInterfaceButton!
+  @IBOutlet weak var buttonTwo: WKInterfaceButton!
+  @IBOutlet weak var buttonThree: WKInterfaceButton!
+  @IBOutlet weak var buttonFour: WKInterfaceButton!
   
+  func randomNumber() -> NSNumber {
+    let number = Int(arc4random_uniform(4)+1)
+    return number
+  }
+  
+  func makeButtonActive(number:NSNumber) {
+    switch number {
+    case 1:
+      setInactive(buttonFour)
+      setInactive(buttonThree)
+      setInactive(buttonTwo)
+      setActive(buttonOne)
+    case 2:
+      setInactive(buttonFour)
+      setInactive(buttonThree)
+      setActive(buttonTwo)
+      setInactive(buttonOne)
+    case 3:
+      setInactive(buttonFour)
+      setActive(buttonThree)
+      setInactive(buttonTwo)
+      setInactive(buttonOne)
+    case 4:
+      setActive(buttonFour)
+      setInactive(buttonThree)
+      setInactive(buttonTwo)
+      setInactive(buttonOne)
+    default:
+      setInactive(buttonFour)
+      setInactive(buttonThree)
+      setInactive(buttonTwo)
+      setActive(buttonOne)
+    }
+  }
+  
+  func setActive(button:WKInterfaceButton) {
+    button.setBackgroundColor(WHITE)
+    button.setTitle("Active")
+    button.setEnabled(true)
+  }
+  
+  func setInactive(button:WKInterfaceButton) {
+    button.setBackgroundColor(BLACK)
+    button.setTitle("Inactive")
+    button.setEnabled(false)
+  }
+  
+  @IBAction func startTimer() {
+    
+  }
   @IBAction func TapOne() {
     label.setText("One was tapped.")
+    random = randomNumber()
+    makeButtonActive(random)
   }
   @IBAction func TapTwo() {
     label.setText("Two was tapped.")
+    random = randomNumber()
+    makeButtonActive(random)
   }
   @IBAction func TapThree() {
     label.setText("Three was tapped.")
+    random = randomNumber()
+    makeButtonActive(random)
   }
   @IBAction func TapFour() {
     label.setText("Four was tapped.")
+    random = randomNumber()
+    makeButtonActive(random)
   }
-  func update() {
-    // This will be the timer loop.
-  }
+
   override func awakeWithContext(context: AnyObject?) {
     super.awakeWithContext(context)
-    // Configure interface objects here.
+    highScore = 0
+    label.setText("Score: \(highScore)")
   }
 }
